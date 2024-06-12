961a3056d131a78dbd3af7da775c46f333eb69bd
saraxplorer <147872964+saraxplorer@users.noreply.github.com>
2024-06-12 16:23:13 +0200

    Add files via upload

--- add_to_window.c
+++ add_to_window.c
@@ -5,8 +5,8 @@
 /*                                                     +:+                    */
 /*   By: rshaheen <rshaheen@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2024/05/06 18:24:11 by rshaheen      #+#    #+#                 */
-/*   Updated: 2024/05/28 15:50:04 by rshaheen      ########   odam.nl         */
+/*   Created: 2024/06/12 10:55:51 by rshaheen      #+#    #+#                 */
+/*   Updated: 2024/06/12 11:07:30 by rshaheen      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,48 +23,63 @@ void	background_to_window(t_game *game)
 		column = 0;
 		while (column < game->width)
 		{
-			if (mlx_image_to_window(game->mlx, game->img->background, column * PIXELS, row * PIXELS) < 0)
+			if (mlx_image_to_window(game->mlx, game->img->background,
+					column * PIXELS, row * PIXELS) < 0)
 				error_exit("cannot bring background to window");
-			column++;	
+			column++;
 		}
 		row++;
 	}
 }
 
-void	add_images(t_game *game, size_t row, size_t column)
+void	wall_exit_to_window(t_game *game, size_t row, size_t column)
 {
 	if (game->grid[row][column] == '1')
 	{
 		if (mlx_image_to_window(game->mlx, game->img->wall,
-							column* PIXELS, row * PIXELS) < 0)
-			error_exit("cannot bring wall to window");	
+				column * PIXELS, row * PIXELS) < 0)
+			error_exit("cannot bring wall to window");
 	}
-	 if (game->grid[row][column] == 'P')
+	else if (game->grid[row][column] == 'E')
+	{
+		if (mlx_image_to_window(game->mlx, game->img->exit,
+				column * PIXELS, row * PIXELS) < 0)
+			error_exit("cannot bring exit to window");
+	}
+}
+
+void	add_images(t_game *game, size_t row, size_t column)
+{
+	if (game->grid[row][column] == 'P')
 	{
 		if (mlx_image_to_window(game->mlx, game->img->player,
-							 column * PIXELS, row * PIXELS) < 0)
+				column * PIXELS, row * PIXELS) < 0)
 			error_exit("cannot bring player to window");
 	}
 	else if (game->grid[row][column] == 'C')
 	{
 		if (mlx_image_to_window(game->mlx, game->img->geld,
-							 column * PIXELS, row * PIXELS) < 0)
+				column * PIXELS, row * PIXELS) < 0)
 			error_exit("cannot bring collectable to window");
 	}
-	else if (game->grid[row][column] == 'E')
-	{
-		if (mlx_image_to_window(game->mlx, game->img->exit,
-							 column * PIXELS, row * PIXELS) < 0)
-			error_exit("cannot bring exit to window");
-	}
-	
 }
 
-void images_to_window(t_game *game)
+void	images_to_window(t_game *game)
 {
 	size_t	row;
 	size_t	column;
-	
+
+	row = 0;
+	while (row < game->height)
+	{
+		column = 0;
+		while (column < game->width)
+		{
+			wall_exit_to_window(game, row, column);
+			column++;
+		}
+		row++;
+	}
 	row = 0;
 	while (row < game->height)
 	{
@@ -77,4 +92,3 @@ void images_to_window(t_game *game)
 		row++;
 	}
 }
-
